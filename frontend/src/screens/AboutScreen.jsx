1f9dd7434e45f7704aeb806b68a44f0673cd0188
Umesh Chandra <umesh.sakinala20@gmail.com>
2023-11-17 08:19:49 +0530

    Added Backend and Frontend

---
+++ frontend/src/screens/AboutScreen.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./AboutScreen.css";
+import umesh from "../assets/images/umesh.jpg";
+import prudhvi from "../assets/images/prudhvi.jpg";
+import mukesh from "../assets/images/mukesh.jpg";
+import pavan from "../assets/images/pavan.jpg";
+import vamsi from "../assets/images/vamsi.png";
+function Members() {
+    return (
+        <div className="main">
+            <div className="title">
+                <h2> Team Members </h2>``
+            </div>
+            <div className="row row-cols-2 row-cols-md-3">
+                <div className=" col card">
+                    <div className="image">
+                        <img src={vamsi} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="details">
+                            <h3>
+                                Chillara V L N S Pavana Vamsi <br />{" "}
+                                <span> Frontend and Backend </span>
+                                <br />
+                                <span>
+                                    <a href="https://pavana-vamsi.vercel.app/">Portfolio</a>{" "}
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div className=" col card">
+                    <div className="image">
+                        <img src={umesh} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="details">
+                            <h3>
+                                Umesh Chandra S <br /> <span> Frontend and Backend </span>
+                                <br />
+                                <span>
+                                    <a href="#">Portfolio</a>{" "}
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div className=" col card">
+                    <div className="image">
+                        <img src={prudhvi} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="details">
+                            <h3>
+                                Pruthivi Raj V S Vulapalli
+                                <br /> <span> Frontend and Backend </span>
+                                <br />
+                                <span>
+                                    <a href="#">Portfolio</a>{" "}
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div className=" col card">
+                    <div className="image">
+                        <img src={mukesh} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="details">
+                            <h3>
+                                Madepalli Mukesh Sai
+                                <br /> <span> Frontend and Backend </span>
+                                <br />
+                                <span>
+                                    <a href="#">Portfolio</a>{" "}
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="col card">
+                    <div className="image">
+                        <img src={pavan} alt="" />
+                    </div>
+                    <div className="content">
+                        <div className="details">
+                            <h3>
+                                Pavan Sai Kishor B <br /> <span> Frontend and Backend </span>
+                                <br />
+                                <span>
+                                    <a href="#">Portfolio</a>{" "}
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default Members;
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Product Helpers eCommerce Platform (v2)
+
+> eCommerce platform built with the MERN stack & Redux.
+
+<img src="./frontend/public/images/screens.png">
+
+<!-- toc -->
+
+- [Features](#features)
+<!-- tocstop -->
+
+## Features
+
+- Full featured shopping cart
+- Product reviews and ratings
+- Top products carousel
+- Product pagination
+- Product search feature
+- User profile with orders
+- Admin product management
+- Admin user management
+- Admin Order details page
+- Mark orders as delivered option
+- Checkout process (shipping, payment method, etc)
+- Database seeder (products & users)
