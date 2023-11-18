abae0ef82a3042dac104c5815f8b48f9b7fae84c
Pvamsi02 <cvlnspvamsi2003d@gmail.com>
2023-11-18 12:13:57 +0530

    Updated URL link

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/'
-    : 'https://product-helpers.onrender.com';
+    ? 'https://product-helper.onrender.com/'
+    : '';
 // console.log(BASE_URL);
 // export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products';
